9cb773779a9ef7893c9772f9af53590dcc131eaa
mclima123 <32933624+mclima123@users.noreply.github.com>
2018-05-13 13:40:01 +0100

    remove unnecessary imports

--- app/src/main/java/com/example/marco/smartcop/InformationLaws.java
+++ app/src/main/java/com/example/marco/smartcop/InformationLaws.java
@@ -2,7 +2,6 @@ package com.example.marco.smartcop;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
