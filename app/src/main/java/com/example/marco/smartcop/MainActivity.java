859f584812949a37662c817fca99715028ce0748
mclima123 <32933624+mclima123@users.noreply.github.com>
2018-05-13 13:24:45 +0100

    initial commit

--- app/src/main/java/com/example/marco/smartcop/MainActivity.java
+++ app/src/main/java/com/example/marco/smartcop/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
